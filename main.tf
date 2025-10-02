b2fc683d09c276ea660d02bc08dfec86dae48072
TEK Student <TEKstudent@vlab.lan>
2025-10-02 09:24:36 -0700

    Intentional violation: unencrypted EBS volume

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ module "lb_security_group" {
 }
 
 resource "aws_ebs_volume" "unencrypted" {
-  availability_zone = "us-west-1a"
+  availability_zone = "us-west-1"
   size              = 8
   encrypted         = false # Intentional violation: unencrypted EBS volume
 }
